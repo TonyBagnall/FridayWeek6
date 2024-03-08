89ec357c2c56e70b10870754344968572b646be4
Tony Bagnall <ajb@uea.ac.uk>
2024-03-08 10:04:02 +0000

    toggle buttons

--- app/src/main/java/uk/ac/soton/comp1206/SliderExample.java
+++ app/src/main/java/uk/ac/soton/comp1206/SliderExample.java
@@ -1,17 +1,24 @@
 package uk.ac.soton.comp1206;
 import javafx.application.Application;
+import javafx.beans.property.StringProperty;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.geometry.Orientation;
+import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.Slider;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class SliderExample extends Application {
-    enum SliderType{STATIC,INNER,LOCAL,ANON,LAMBDA}
+    enum SliderType{STATIC,INNER,LOCAL,ANON,LAMBDA,FUNCTION}
+    static SliderType nested=SliderType.ANON;
+    static String message="Set with "+nested;
+
     @Override
     public void start(Stage primaryStage) {
         // Create a slider with a range from 0 to 100 with an initial value of 50
@@ -24,22 +31,43 @@ public class SliderExample extends Application {
         // Create a label that will display the value of the slider
         slider.setStyle("-fx-font-size: 24px;");
         // Lambda example
-        Label sliderValue = setSlider(slider, SliderType.ANON);
 
 
+        VBox toggles = makeToggleButtons();
         // Arrange the slider and label vertically
-        VBox root = new VBox(10, slider, sliderValue);
-
-        Scene scene = new Scene(root, 400, 200);
+        HBox root = new HBox();
+        TextArea sliderValue = setSlider(slider);
+        sliderValue.setPrefSize(400,10);
+        VBox s = new VBox(10, slider, sliderValue);
+        root.getChildren().addAll(s,toggles);
+        Scene scene = new Scene(root, 600, 200);
 
         primaryStage.setTitle("Slider Example");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
-    private static Label setSlider(Slider s, SliderType t){
-        Label sliderValue = new Label(Double.toString(s.getValue()));
+    private static VBox makeToggleButtons(){
+        VBox container = new VBox();
+        container.setSpacing(10); // Set spacing between elements
+        // Create a ToggleGroup
+        ToggleGroup group = new ToggleGroup();
+        RadioButton[] buttons = new RadioButton[5];
+        String[] names = {"Inner Class", "Static nested class", "anonymous class","Lambda","Function reference"};
+        for (int i=0;i<5;i++) {
+            buttons[i] = new RadioButton(names[i]);
+            buttons[i].setToggleGroup(group);
+            final int x = i;
+            buttons[i].setOnAction(a -> nested = SliderType.values()[x]);
+            container.getChildren().add(buttons[i]);
+        }
+        return container;
+
+    }
+
+    private static TextArea setSlider(Slider s){
+        TextArea sliderValue = new TextArea(Double.toString(s.getValue()));
         sliderValue.setFont(new Font(24));
-        switch(t){
+        switch(nested){
             case STATIC:
                 break;
             case INNER:
@@ -50,7 +78,8 @@ public class SliderExample extends Application {
                 s.valueProperty().addListener(new ChangeListener<Number>() {
                     @Override
                     public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
-                        sliderValue.setText(String.format("Set with anon class %.2f", newValue));
+                        sliderValue.setText(String.format(message+" %.2f", newValue));
+                        System.out.println(" Value of message = "+message+" value of nested = "+nested);
                     }
                 });
                 break;
@@ -61,11 +90,10 @@ public class SliderExample extends Application {
 //                });
                 //Lambda without typing, which is inferred
                 s.valueProperty().addListener((ov, old_val, new_val) -> {
-                    sliderValue.setText(String.format("Set with Lambda = %.2f", new_val));
+                    sliderValue.setText(String.format(message+" = %.2f", new_val));
                 });
                 break;
         }
-
         return sliderValue;
     }
 
